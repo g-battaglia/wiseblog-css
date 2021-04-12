c71d5960b3cbb69850b2188289eef4c9db23fabc
g-battaglia <battaglia.giacomo@icloud.com>
2021-04-12 02:56:07 +0200

    Navbar completed.

--- script.js
+++ script.js
@@ -1,13 +1,64 @@
-const search = document.querySelector('.navbar-search')
-const searchOverlay = document.querySelector('.search-overlay')
-const closeOverlay = document.querySelector('.close-overlay')
+// const search = document.querySelector('.navbar-search')
+// const searchOverlay = document.querySelector('.search-overlay')
+// const closeOverlay = document.querySelector('.close-overlay')
 
-search.addEventListener("click", function() {
-    console.log('ok');
-    searchOverlay.classList.toggle("hide-overlay");
-})
+// search.addEventListener("click", function() {
+//     console.log('ok');
+//     searchOverlay.classList.toggle("hide-overlay");
+// })
 
-closeOverlay.addEventListener("click", function() {
-    console.log('ok');
-    searchOverlay.classList.toggle("hide-overlay");
-})
+// closeOverlay.addEventListener("click", function() {
+//     console.log('ok');
+//     searchOverlay.classList.toggle("hide-overlay");
+// })
+
+function toggler(openIconClass, overlayClass, closeIconClass, hideClassJustName) {
+    const search = document.querySelector(openIconClass)
+    const searchOverlay = document.querySelector(overlayClass)
+    const closeOverlay = document.querySelector(closeIconClass)
+
+    search.addEventListener("click", function() {
+        searchOverlay.classList.toggle(hideClassJustName);
+    })
+
+    closeOverlay.addEventListener("click", function() {
+        searchOverlay.classList.toggle(hideClassJustName);
+})}
+
+toggler(".navbar-search",
+        ".search-overlay",
+        ".close-overlay",
+        "hide-overlay"
+    )
+
+toggler(".navbar-toggler",
+        ".under-nav",
+        ".close-nav-overlay",
+        "hide-overlay-nav"
+)
+
+
+
+function hideDropdownMenu(togleClass, iconClass, animatedClassName, menuClass, menuHide) {
+    const t = document.querySelector(togleClass)
+    const i = document.querySelector(iconClass)
+    const mc = document.querySelector(menuClass)
+    const mh  =  menuHide
+
+    const animateClassName = animatedClassName
+
+    t.addEventListener("click", function() {
+        i.classList.toggle("navbar-link__rotate");
+    })
+
+    t.addEventListener("click", function() {
+        mc.classList.toggle(mh);
+    })
+}
+
+hideDropdownMenu(".dropdown-toggler",
+               ".dropdown-link",
+               "navbar-link__rotate",
+               ".dropdown-menu",
+               "hide-dropdown-menu"
+)
